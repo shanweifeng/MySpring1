dbfecd31f8a76bc3e3265327c7bf0ba5d723f3cd
shanweifeng <867390626@qq.com>
2017-07-31 00:52:26 +0800

    测试上传到GitHub上面

--- src/test/java/com/swf/web/controller/Test.java
+++ src/test/java/com/swf/web/controller/Test.java
@@ -7,5 +7,7 @@ public class Test {
     public static void main(String[] args){
         System.out.println("千日幻想不如一日行动。要么化龙，要么死在化龙的路上。疯");
         System.out.println("测试上传到github上面");
+        String str = "this my firt project,and start work.";
+        System.out.println(str);
     }
 }
