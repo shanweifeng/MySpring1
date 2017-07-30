349c2ad2effcf98bb20a396318818903328378fc
单伟峰 <shanwf@gegejia.com>
2017-07-31 00:36:56 +0800

    mySpring

---
+++ src/test/java/com/swf/web/controller/Test.java
@@ -0,0 +1,10 @@
+package com.swf.web.controller;
+
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("千日幻想不如一日行动。要么化龙，要么死在化龙的路上。疯");
+    }
+}
